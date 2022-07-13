eea556d489fb57199481ccc37bb83e537ba35d25
Carlos William <carloswtmachado@gmail.com>
2022-07-13 20:54:10 -0300

    post poll valida 'expireAt' vazia

--- middlewares/pollMiddleware.js
+++ middlewares/pollMiddleware.js
@@ -5,7 +5,7 @@ import {ObjectId} from 'mongodb';
 export async function validaPostPoll(req, res, next) {
 	const schema = joi.object({
 		title: joi.string().required(),
-		expireAt: joi.string()
+		expireAt: joi.string().allow('')
 	});
 	const validation = schema.validate(req.body);
 	if(validation.error ) return res.status(422).send(validation.error.details);
